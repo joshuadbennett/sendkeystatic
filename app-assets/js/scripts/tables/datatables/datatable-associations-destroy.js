219c4d7277574204cddbfa156ab792b6597ceb0a
Joshua Bennett <jbennett@keycentrix.com>
2022-01-11 11:39:35 -0600

    cleared

--- app-assets/js/scripts/tables/datatables/datatable-associations-destroy.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    var tableToDestroy = $('.sendkey-assoc').DataTable();
-    console.log(tableToDestroy);
-    if (tableToDestroy != null) {
-        tableToDestroy.destroy(false);
-    }
-});
